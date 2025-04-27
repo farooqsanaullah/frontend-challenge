07877e1ca3bcd34cdf61f034fe1de6e524014a98
farooqsanaullah <farooqsanaullahs@gmail.com>
2025-04-27 18:46:28 +0500

    REMOVED: deletes all old files

--- src/pages/TodoPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TodoList } from '../components/TodoList';
-
-export const TodoPage: React.FC = () => (
-  <div className="min-h-screen max-w-xl mx-auto p-4 sm:p-6 lg:p-8">
-    <TodoList />
-  </div>
-);
